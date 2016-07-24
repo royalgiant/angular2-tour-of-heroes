da0030c988bbf32e238c8a1894383250f8e6ec8d
Donald Lee <donaldlee50@gmail.com>
2016-07-23 20:49:03 -0500

    Angular2 Tutorial Done

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service'; 
 import { Router } from '@angular/router';
+import { HeroSearchComponent } from './hero-search.component';
 
 @Component({
   selector: 'my-dashboard',
   templateUrl: 'app/dashboard.component.html',
-  styleUrls: ['app/dashboard.component.css']
+  styleUrls: ['app/dashboard.component.css'],
+  directives: [HeroSearchComponent]
 })
-export class DashboardComponent { 
+export class DashboardComponent implements OnInit { 
+	
 	heroes: Hero[] = []; 
 
 	constructor(
